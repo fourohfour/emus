50d265d88cf80c6299d4d594a1fb4128fdfa9763
FourOhFour <404.fourohfour@gmail.com>
2017-05-28 22:30:11 +0100

    Better text output code

--- text.py
+++ text.py
@@ -1,45 +1,69 @@
 import interface
 
-class TextEmitter(interface.Emitter):
-    def textify(self, table):
-        col_widths = {}
+class TableWidth:
+    FIRST_COL = -1
+
+    def __init__(self, table):
+        col_widths = {TableWidth.FIRST_COL: 0}
+
+        for row in table.rows():
+            row_name = str(row)
+            if len(row_name) > col_widths[TableWidth.FIRST_COL]:
+                col_widths[TableWidth.FIRST_COL] = len(row_name)
+
         for col in table.columns():
-            col_widths[str(col)] = len(str(col))
+            col_name = str(col)
+            col_widths[col_name] = len(col_name)
             for row, value in col:
-                if len(str(value)) > col_widths[str(col)]:
-                    col_widths[str(col)] = len(str(value))
+                if len(str(value)) > col_widths[col_name]:
+                    col_widths[col_name] = len(str(value))
 
-        s = "|"
-        score = "|"
-        rh_width = 0
+        self.col_widths = col_widths
 
-        for row in table.rows():
-            if len(str(row)) > rh_width:
-                rh_width = len(str(row))
+    def col_width(self, c):
+        return self.col_widths[c]
 
-        rh_width += 1
+    def first_col_width(self):
+        return self.col_width(TableWidth.FIRST_COL)
 
-        s += (" " * rh_width) + "|"
-        score += ("-" * rh_width)
+    def table_width(self):
+        return sum(self.col_widths.values()) + len(self.col_widths) + 1
 
+
+class TextEmitter(interface.Emitter):
+    def h_border(self, width):
+        return "+" + ("-" * (width.table_width() - 2)) + "+\n"
+
+    def h_title(self, width, table):
+        spaces = (width.table_width() // 2) - (len(table.title()) // 2)
+        return (" " * spaces) + table.title() + "\n"
+
+    def textify(self, table):
+        master = ""
+        width = TableWidth(table)
+        master += self.h_title(width, table)
+        master += self.h_border(width)
+
+        master += "|" + (" " * width.first_col_width()) + "|"
         for col in table.columns():
-            s += "{head: <{w}}|".format(head = str(col), w = col_widths[str(col)])
-            score += "+" + ("-" * col_widths[str(col)])
-        score += "|\n"
-        hborder = "+" +  "-" * (len(score) - 3) + "+\n"
+            cw = width.col_width(str(col))
+            master += "{head: <{w}}|".format(head = str(col), w = cw)
+        master += "\n"
 
-        s  = hborder + s + "\n" + score
+        col_names = [TableWidth.FIRST_COL] + [str(col) for col in table.columns()]
+
+        master += "|" + "+".join(width.col_width(col) * "-" for col in col_names) + "|\n"
 
         for row in table.rows():
-            s += "|{head: <{rhw}}|".format(head = str(row), rhw = rh_width)
+            master += "|{head: <{fcw}}|".format(head = str(row), fcw = width.first_col_width())
             for col in table.columns():
-                s += "{val: <{w}}|".format(val = row[str(col)], w = col_widths[str(col)])
-            s += "\n"
+                col_name = str(col)
+                cw = width.col_width(col_name)
+                master += "{val: <{w}}|".format(val = row[col_name], w = cw)
+            master += "\n"
 
-        s  += hborder
-
-        return s
-
+        master += self.h_border(width)
+        return master
 
     def emit(self):
         for table in self.data():
